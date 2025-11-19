a38753f967758c0db4952405618da003a911af87
Alexander Lyukshin <alexluni22@gmail.com>
2025-11-19 18:35:33 +0300

    Add html and css prototypes

--- cafes/views.py
+++ cafes/views.py
@@ -1,5 +1,36 @@
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello from Coffee Guide!")
+    cafes = [
+        {
+            "name": "Brew & Chill",
+            "tagline": "Спешелти-кофе и тихая музыка.",
+            "city": "Москва",
+            "address": "ул. Примерная, 10",
+            "average_bill": 350,
+            "format": "спешелти",
+        },
+        {
+            "name": "City Coffee",
+            "tagline": "Забегай по пути на работу.",
+            "city": "Москва",
+            "address": "пр-т Центральный, 5",
+            "average_bill": 250,
+            "format": "to go",
+        },
+        {
+            "name": "Латте & Книги",
+            "tagline": "Кофе и полки с любимыми книгами.",
+            "city": "Москва",
+            "address": "наб. Речная, 3",
+            "average_bill": 400,
+            "format": "кофейня + коворкинг",
+        },
+    ]
+
+    context = {
+        "cafes": cafes,
+    }
+    return render(request, "cafes/home.html", context)
